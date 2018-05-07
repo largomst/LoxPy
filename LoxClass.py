44b50eace717f4ac5f639cd3b7aee8c23c85fc97
largmost <dingrui.zhen@gmail.com>
2018-05-07 14:50:43 +0800

    create instances

--- LoxClass.py
+++ LoxClass.py
@@ -1,6 +1,26 @@
-class LoxClass:
+from typing import List
+
+from LoxCallable import LoxCallable
+
+
+class LoxClass(LoxCallable):
     def __init__(self, name: str):
         self.name = name
 
     def __str__(self):
         return self.name
+
+    def call(self, interpreter: "Interpreter", arguments: List[object]):
+        instance = LoxInstance(self)
+        return instance
+
+    def arity(self):
+        return 0
+
+
+class LoxInstance:
+    def __init__(self, class_: LoxClass):
+        self.class_ = class_
+
+    def __str__(self):
+        return self.class_.name + ' instance'
